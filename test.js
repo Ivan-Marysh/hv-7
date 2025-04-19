797e8da8c993c17fc19acc15ad109b1990748475
Ivan-Marysh <166243361+Ivan-Marysh@users.noreply.github.com>
2025-04-19 16:23:42 +0300

    add 13

---
+++ test.js
@@ -0,0 +1,19 @@
+const number = [5, 11, 15];
+number[1] = 10;
+console.log(number);
+
+
+
+const text = ["text", "banana", "mango"];
+text[3] = "apple";
+console.log(text);
+
+
+
+const numbers = [76, 245, 16, 150];
+let guma = 0;
+for(let i = 0; i < numbers.length; i++){
+    
+    guma += numbers[i]
+}
+console.log(guma);
