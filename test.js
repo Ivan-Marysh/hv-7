def7468381db698e924b476705f434caa7be8b2d
Ivan-Marysh <166243361+Ivan-Marysh@users.noreply.github.com>
2025-04-20 19:07:26 +0300

    add 65

--- test.js
+++ test.js
@@ -1,19 +1,41 @@
-const number = [5, 11, 15];
-number[1] = 10;
-console.log(number);
+// const number = [5, 11, 15];
+// number[1] = 10;
+// console.log(number);
 
 
 
-const text = ["text", "banana", "mango"];
-text[3] = "apple";
-console.log(text);
+// const text = ["text", "banana", "mango"];
+// text[3] = "apple";
+// console.log(text);
 
 
 
-const numbers = [76, 245, 16, 150];
-let guma = 0;
-for(let i = 0; i < numbers.length; i++){
+// const numbers = [76, 245, 16, 150];
+// let guma = 0;
+// for(let i = 0; i < numbers.length; i++){
     
-    guma += numbers[i]
-}
-console.log(guma);
+//     guma += numbers[i]
+// }
+// console.log(guma);
+
+
+
+
+
+// const number3 = [189, 528, 347, 876, 656];
+// for (let i = 0; i < number3.length; i++) {
+//     console.log(cart[i]);
+    
+// }
+
+
+
+// const omega = ["mather", "danger", "plan", "dead", "color"];
+// for (let i = 0; i < omega.length; i++) {
+//     if (omega[i].length > 5) {
+//         console.log(omega[i]);
+//     }
+    
+// }
+
+
